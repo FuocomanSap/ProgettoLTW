c3d9f30e9b0ff7980b52db08cf903a427ffda03e
RickyMexx <riccardo.caprari@gmail.com>
2018-11-18 20:13:43 +0100

    token added

--- template/aut.php
+++ template/aut.php
@@ -4,9 +4,9 @@
     $password = $_POST['password'];
 
     if ($email =='riccardo.caprari@gmail.com' and $password =='123') {
-        header('Location: afterloginindex.html?name=Ricky'); //include 'index.html';
+        header('Location: afterloginindex.html?name=Ricky&token=kQ94is3PRPPgry01JK2e'); //include 'index.html';
     } else if ($email =='fuocoman96@gmail.com' and $password =='123') {
-        header('Location: afterloginindex.html?name=Francesco');
+        header('Location: afterloginindex.html?name=Francesco&token=kQ94is3PRPPgry01JK2e');
     } else {
         echo 'Non sei registrato!';
     }
