6c3018ae624ece29775bea363cda77099b4c6fe6
RickyMexx <riccardo.caprari@gmail.com>
2018-11-18 19:47:58 +0100

    mega fixes on php

--- template/aut.php
+++ template/aut.php
@@ -3,12 +3,11 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    if ($email =='riccardo.caprari@gmail.com' and $password =='123')
-    {
-        include 'index.html';
-    }
-    else
-    {
-        echo 'you are not the admin';
+    if ($email =='riccardo.caprari@gmail.com' and $password =='123') {
+        header('Location: afterloginindex.html?name=Ricky'); //include 'index.html';
+    } else if ($email =='fuocoman96@gmail.com' and $password =='123') {
+        header('Location: afterloginindex.html?name=Francesco');
+    } else {
+        echo 'Non sei registrato!';
     }
     ?>
