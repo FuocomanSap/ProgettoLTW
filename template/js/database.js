b61da224e567c1938c2818a5eba0723724fc9f4e
francesco douglas scotti di vigoleno <douglasscottidivigoleno.1743635@studenti.uniroma1.it>
2018-11-19 21:02:24 +0100

    errori nel database

--- template/js/database.js
+++ template/js/database.js
@@ -10,12 +10,14 @@ function isRegistered(email) {
     return true;
 }
 
-function RegisterUser(form){
-    window.alert(form);
+function RegisterUser(){
+     
+    var persona = ('Nome':document.registerForm.name.value,'Cognome':( document.registerForm.urname.value), 'Password': document.registerForm.password.value,'Numero di Telefono': document.registerForm.phoneNumber.value,'Indirizzo': document.registerForm.address.value, 'Codice Fiscale': document.registerForm.CF.value,'Data di Nscita': document.registerForm.birthday.value,'Luogo di Nascita': document.registerForm.birthcountry.value,'Esenzione': document.registerForm.category.value);
+    localStorage.setItem(form.email.value,JSON.stringify(persona));
+    window.alert(persona);
+    window.alert(JSON.stringify(persona));
+
     return true;
-
-
-
 }
 
 function whoIsLogged() {
