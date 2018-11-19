f0ff1beda949ce99286c809c5fd2e9deb02b78f5
RickyMexx <riccardo.caprari@gmail.com>
2018-11-19 20:56:40 +0100

    new php authentication, login and afterlogin

--- template/aut.php
+++ template/aut.php
@@ -1,13 +1,12 @@
 <?php
-
+    header('Location: afterloginindex.html');
+    
+    /*
     $email = $_POST['email'];
     $password = $_POST['password'];
 
     if ($email =='riccardo.caprari@gmail.com' and $password =='123') {
-        header('Location: afterloginindex.html?name=Riccardo&key=kQ94is3PRPPgry01JK2e'); //include 'index.html';
-    } else if ($email =='fuocoman96@gmail.com' and $password =='123') {
-        header('Location: afteradminloginindex.html?name=Dr. Francesco&key=kQ94is3PRPPgry01JK2e');
-    } else {
-        echo 'Non sei registrato!';
+        header('Location: afterloginindex.html'); //include 'index.html';
     }
-    ?>
+    */
+?>
