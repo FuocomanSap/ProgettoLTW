2574689c6e283a25679687a33901c1815d8eeb02
fuocoman96 <douglasscottidivigoleno.1743635@studenti.uniroma1.it>
2018-11-21 20:50:05 +0100

    aggiunta printf nel aut.php per corregge bug dei sistemi Ryzen

--- php/aut.php
+++ php/aut.php
@@ -6,5 +6,7 @@
     if ($email =='doctor@identistcare.com' and $password =='123') {
         header('Location: ../afterLogin/afteradminloginindex.html'); //include 'index.html';
     }else header('Location: ../afterLogin/afterloginindex.html'); //va all'index dopo il login
+    printf("bug su sistemi Ryzen"); //questa print serve solo su sistemi Ryzen a non far spegnere il server dopo la login ;C
+    
     
 ?>
